a809d69d6e04ee740ac632ae261ea374163f6020
teemutin <80262041+teemutin@users.noreply.github.com>
2021-11-05 00:49:39 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+if (document.readyState !== "loading") {
+  console.log("Doc rdy");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Doc rdy soon");
+  });
+}
+function initializeCode() {
+  const addCommentButton = document.getElementById("add-comment");
+
+  addCommentButton.addEventListener("click", function () {
+    const addComment = document.getElementById("comments");
+    addComment.innerHTML = addComment.innerHTML + "hei\t";
+  });
+}
+
+/*
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+*/
